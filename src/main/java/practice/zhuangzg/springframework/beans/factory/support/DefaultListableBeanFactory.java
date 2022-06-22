cfcee30fb33979a59380b08c16b6a924833ac620
ZhuangZG <18270806346@163.com>
2022-06-22 13:52:10 +0800

    5 资源加载器解析文件注册对象

--- src/main/java/practice/zhuangzg/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ src/main/java/practice/zhuangzg/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -2,6 +2,7 @@ package practice.zhuangzg.springframework.beans.factory.support;
 
 import practice.zhuangzg.springframework.beans.BeansException;
 import practice.zhuangzg.springframework.beans.factory.BeanFactory;
+import practice.zhuangzg.springframework.beans.factory.ConfigurableListableBeanFactory;
 import practice.zhuangzg.springframework.beans.factory.config.BeanDefinition;
 
 import java.util.HashMap;
@@ -13,7 +14,7 @@ import java.util.Objects;
  * @date 2022/6/16 22:57
  * @Description:
  */
-public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry{
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry, ConfigurableListableBeanFactory {
 
     private final Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
@@ -39,5 +40,17 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
     @Override
     public String[] getBeanDefinitionNames() {
         return beanDefinitionMap.keySet().toArray(new String[0]);
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        Map<String, T> result = new HashMap<>();
+        beanDefinitionMap.forEach((beanName, beanDefinition) -> {
+            Class beanClass = beanDefinition.getBeanClass();
+            if (beanClass.isAssignableFrom(type)) {
+                result.put(beanName, (T)getBean(beanName));
+            }
+        });
+        return result;
     }
 }
